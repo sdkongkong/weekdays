69a7e908bb692100159260f5cf7be7dabe1325d0
kevinsong <xingchao.song@gmail.com>
2019-04-14 14:47:21 +1000

    add testcase do deal with weekend

--- data/src/test/java/com/kevinsong/data/TestCalculator.kt
+++ data/src/test/java/com/kevinsong/data/TestCalculator.kt
@@ -1,12 +1,23 @@
 package com.kevinsong.data
 
+import org.junit.Assert
 import org.junit.Test
+import sun.util.calendar.BaseCalendar
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class TestCalculator {
 
     @Test
-    fun testNewYear() {
+    fun testWithOutWeekend() {
+        val startStr = "11/04/2019"
+        val endStr = "16/04/2019"
 
+        val curFormater = SimpleDateFormat("dd/MM/yyyy")
+        val startDate = curFormater.parse(startStr)
+        val endDate = curFormater.parse(endStr);
+        val calc = Calculator(startDate, endDate)
+        Assert.assertEquals(calc.getWeekDays(), 2)
     }
 }
