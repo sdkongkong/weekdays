878ce52e802a14d5dd4570c04f4d52a79ea327dd
kevinsong <xingchao.song@gmail.com>
2019-04-14 14:24:48 +1000

    init project

---
+++ data/src/main/java/com/kevinsong/data/Calculator.kt
@@ -0,0 +1,4 @@
+package com.kevinsong.data
+
+class Calculator {
+}
---
+++ data/src/test/java/com/kevinsong/data/TestCalculator.kt
@@ -0,0 +1,12 @@
+package com.kevinsong.data
+
+import org.junit.Test
+
+
+class TestCalculator {
+
+    @Test
+    fun testNewYear() {
+
+    }
+}
