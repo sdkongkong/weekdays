3ea0be00d062235aa58021edc319a4296d4b920d
kevinsong <xingchao.song@gmail.com>
2019-04-14 15:04:39 +1000

    finish the method body dealing weekend

--- data/src/main/java/com/kevinsong/data/Calculator.kt
+++ data/src/main/java/com/kevinsong/data/Calculator.kt
@@ -1,4 +1,19 @@
 package com.kevinsong.data
 
-class Calculator {
+import java.util.*
+
+class Calculator(val start:Date, val end:Date) {
+
+    public fun getWeekDays():Int{
+       val cStart = Calendar.getInstance()
+        cStart.time = start
+        val cEnd = Calendar.getInstance()
+        cEnd.time = end
+        val wStart= cStart.get(Calendar.DAY_OF_WEEK)
+        cStart.add(Calendar.DAY_OF_WEEK, -wStart)
+        val wEnd = cEnd.get(Calendar.DAY_OF_WEEK)
+        cEnd.add(Calendar.DAY_OF_WEEK,-wEnd)
+        return wStart - wEnd
+    }
+
 }
